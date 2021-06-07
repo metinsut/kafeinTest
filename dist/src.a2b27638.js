c278295fbc6cd8ddbe96ade82cc591c1a15a9b65
metinsut <metindir@gmail.com>
2021-06-07 11:20:15 +0300

    app on heroku

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -40171,7 +40171,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60305" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62929" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
